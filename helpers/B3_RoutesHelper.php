9744e31f190a58f4dd7d384422d1951fbd7157d4
Luis Rodrigues <hello@goblindegook.net>
2014-10-07 14:40:58 +0100

    Catchall page routes moved to the end.

--- helpers/B3_RoutesHelper.php
+++ helpers/B3_RoutesHelper.php
@@ -60,10 +60,10 @@ class B3_RoutesHelper {
 		$this->add_search_routes();
 		$this->add_author_routes();
 		$this->add_post_routes();
-		$this->add_page_routes();
 		$this->add_post_type_routes();
 		$this->add_taxonomy_routes();
 		$this->add_date_routes();
+		$this->add_page_routes();
 
 		if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
 			$this->unfold_tokens();
